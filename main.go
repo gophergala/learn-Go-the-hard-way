09b77575d9b1a494d12e00ed6d0a3d91114f40d1
ggaaooppeenngg <471160171@qq.com>
2015-01-24 18:30:05 +0800

    MakeFunc

--- main.go
+++ main.go
@@ -1,81 +1,19 @@
 package main
 
 import (
-	"math/rand"
+	"reflect"
 )
 
-const (
-	ROCK int = iota
-	PAPER
-	SCISSORS
-)
-
-type Choice struct {
-	Who   int //0 you 1 your opponent
-	Guess int
+func MakeMap(fpt interface{}) {
+	fnV := reflect.ValueOf(fpt).Elem()
+	fnI := reflect.MakeFunc(fnV.Type(), implMap)
+	fnV.Set(fnI)
 }
 
-//Win returns true if you win.
-func Win(you, he int) bool {
-	if you == ROCK && he == SCISSORS {
-		return true
-	}
-	if you == PAPER && he == ROCK {
-		return true
-	}
-	if you == SCISSORS && he == PAPER {
-		return true
-	}
-	return false
-}
-
-func Opponent(guess chan Choice, please chan struct{}) {
-	for i := 0; i < 3; i++ {
-		<-please
-		choice := rand.Intn(3)
-		who := 1
-		guess <- Choice{who, choice}
-		please <- struct{}{}
-	}
-}
-
-var Cheat func(guess chan Choice) chan Choice
-
-func Me(guess chan Choice, please chan struct{}) {
-	for i := 0; i < 3; i++ {
-		<-please
-		choice := rand.Intn(3)
-		who := 0
-		guess <- Choice{who, choice}
-		please <- struct{}{}
-	}
-}
-
-func Game() []bool {
-	guess := make(chan Choice)
-	//please sync 2 goroutines.
-	please := make(chan struct{})
-	go func() { please <- struct{}{} }()
-	go Opponent(guess, please)
-	go Me(guess, please)
-	guess = Cheat(guess)
-	var wins []bool
-
-	for i := 0; i < 3; i++ {
-		g1 := <-guess
-		g2 := <-guess
-		win := false
-		if g1.Who == 0 {
-			win = Win(g1.Guess, g2.Guess)
-		} else {
-			win = Win(g2.Guess, g1.Guess)
-		}
-		wins = append(wins, win)
-	}
-
-	return wins
-}
+//TODO:completes implMap function.
+var implMap func([]reflect.Value) []reflect.Value
 
 func main() {
-	println("Now let's play a game 'rock-paper-scissors',there are 2 players-you and a goroutine!\nTo be bound to win,you should call a goroutine to help you to peer what your opponent choose.\nTwo out of three.\nPlease edit main.go to complete func 'Cheat' to win!")
+
+	println("It is said that Go has no generics.\nHowever we have many other ways to implement a generics like library if less smoothly,one is reflect.MakeFunc.\nUnderscore is a very useful js library,and now let's implement part of it-map,it will help you to understand how reflect works.\nPlease finish the 'implMap' function and pass the test.")
 }
--- main_test.go
+++ main_test.go
@@ -4,11 +4,31 @@ import (
 	"testing"
 )
 
-func TestGame(t *testing.T) {
-	win := Game()
-	for _, w := range win {
-		if !w {
-			t.Fail()
-		}
+func TestMap(t *testing.T) {
+	var f func(func(e int) int, []int) []int
+	MakeMap(&f)
+
+	var a = []int{1, 2, 3}
+
+	b := f(func(e int) int {
+		return e + 1
+	}, a)
+
+	if b[0] != 2 || b[1] != 3 || b[2] != 4 || len(b) != 3 {
+		t.Fail()
+	}
+
+	var f2 func(func(e int) int, map[int]int) map[int]int
+
+	MakeMap(&f2)
+
+	var c = map[int]int{0: 0, 1: 1, 2: 3}
+
+	d := f2(func(e int) int {
+		return e + 1
+	}, c)
+
+	if d[0] != 1 || d[1] != 2 || d[2] != 4 || len(d) != 3 {
+		t.Fail()
 	}
 }
