39852d61fb44f1616eaee230aea603c68d4ec509
ggaaooppeenngg <471160171@qq.com>
2015-01-24 10:08:57 +0800

    Reverse slice

--- main.go
+++ main.go
@@ -1,5 +1,8 @@
 package main
 
+//Reverse reverses a slice.
+var Reverse func(slice interface{})
+
 func main() {
-	println("Welcom to 'learn go the hard way'!\nTo get the 1st exercise please run 'git checkout l1'")
+	println("Please edit main.go,and complete the 'Reverse' function to pass the test.\nYou should use reflect package to reflect the slice type and make it applly to any type.\nTo run test,please run 'go test'\nIf you pass the test,please run 'git checkout l2' ")
 }
---
+++ main_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"testing"
+)
+
+type myType int
+
+func TestReverse(t *testing.T) {
+	var slice = []myType{0, 1, 2}
+	Reverse(&slice)
+	if slice[0] != myType(2) && slice[1] != myType(1) && slice[2] != myType(0) && len(slice) != 3 {
+		t.Fail()
+	}
+}
