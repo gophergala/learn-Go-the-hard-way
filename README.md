34d36236231527d1bfa736c73dcc0a9ee384580a
ggaaooppeenngg <471160171@qq.com>
2015-01-25 10:21:15 +0800

    update README.md

--- README.md
+++ README.md
@@ -2,23 +2,24 @@
 
 The best way to learn coding is practice,it is insipred by "learn python the hard way".
 
-There are currently 10 exercises.
+There are currently 10 exercises,all comes from smart parts of open libraries or relevant pappers,it will help you understand Go's features instead of browsing the huge code.
 
 You should complete the current exercise before you enter the next.
 
 Each exercise is a git tag,you can check out the tag,and finish the task with tips.
 
-Run `go test`,if you completes the task,and it will tell you if you pass.
+Run `go test`,if you completes the task,and it will tell you wether you pass.
 
-To get the tips please run `go run main.go`.
+To get the tips,please run `go run main.go`.
 
 Now run `go run main.go`,let's go!
 
 #Categories:
 
-1.  reverse slice
+1.  warm up,reverse slice
 2.  parallel vector sum
 3.  cheat rock-paper-sissors
 4.  make map function
 5.  parallel dynamic programming
-6.  
+6.  tiny webframework 1,managing context.
+7.  tiny webframework 2,middleware.
--- main.go
+++ main.go
@@ -1,112 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 )
 
-const (
-	n  = 5 //scale of the tree.
-	vp = 2 //number of chunks on one demension,totally vp(vp+1)/2
-)
-
-var (
-	cost   [n + 1][n + 1]float64 //cost for tree(i-1,j)
-	root   [n + 1][n + 1]int     //root for tree(i-1,j)
-	prob   [n]float64            //probability for n nodes.
-	h, v   [vp][vp]chan struct{} //horizontal channel and vertical channel.
-	finish chan struct{}         //if reach the chunk(0,vp-1),send finish signal.
-)
-
-//prints from (il,jl) to (ih,jh)
-func P(il, ih, jl, jh int) string {
-	var out string
-	for ii := il; ii <= ih; ii++ {
-		for jj := jl; jj <= jh; jj++ {
-			out += fmt.Sprint(cost[ii][jj], " ")
-		}
-		out += fmt.Sprintln()
-	}
-	return out
+type Server struct {
 }
 
-//get sequential mean search time.
-func MstSeq(i, j int) {
-	var bestCost float64 = 1e9 + 0.0
-	var bestRoot int = -1
-	switch {
-	case i >= j: //empty tree
-		cost[i][j] = 0.0
-		root[i][j] = -1
-	case i+1 == j: //single tree
-		cost[i][j] = prob[i]
-		root[i][j] = i + 1
-	case i+1 < j:
-		psum := 0.0
-		for k := i; k <= j-1; k++ {
-			psum += prob[k]
-		}
-		for r := i; r <= j-1; r++ {
-			rcost := cost[i][r] + cost[r+1][j]
-			if rcost < bestCost {
-				bestCost = rcost
-				bestRoot = r + 1
-			}
-			cost[i][j] = bestCost + psum
-			root[i][j] = bestRoot
-		}
-	}
+func NewServer() *Server {
+	return &Server{}
 }
 
-//compute chunk(i,j)
-var chunk func(i, j int)
-
-func init() {
-	for i := 0; i < vp; i++ {
-		for j := i; j < vp; j++ {
-			if j < vp-1 {
-				h[i][j] = make(chan struct{})
-			}
-			if i > 0 {
-				v[i][j] = make(chan struct{})
-			}
-		}
-	}
-	finish = make(chan struct{})
+//provide context for each request.
+type Context struct {
+	*http.Request
+	http.ResponseWriter
 }
 
 func main() {
-	prob = [n]float64{5, 10, 2, 3, 4}
-	for i := n; i >= 0; i-- {
-		for j := i; j <= n; j++ {
-			MstSeq(i, j)
-		}
-	}
-	if P(0, n, 0, n) == `0 5 20 24 32 44 
-0 0 10 14 22 34 
-0 0 0 2 7 15 
-0 0 0 0 3 10 
-0 0 0 0 0 4 
-0 0 0 0 0 0 
-` {
-		println(`Again,Go is designed for concurrency,it makes it easy to parrallel.This exercise is about parrallel dynamic programming.
-We use the optimal binary search tree problem to show the multi-core parallel code in Go.
-The mean search time for tree(nodes form i+1 to j) is stored in cost[i][j],similarily root of the search tree is stored in root[i][j]
-And a tree mean search time is the total probability of all nodes plus the minium mean time of 2 possible sub trees.
-The sequential MST method has been given in the main.go
-In fact the every cost[i][j] depencies on their left and bottom neighbor.
-So we can divide the computation into chunks like that (assume n is 5):
-  
-  * * *    | * * *
-    * * -> | * * *
-      *    | * * *
-           + - - -
-               ^
-               |
-             * * *
-               * *
-                 *
-
-denotes left-top chunk(0,0),right-top chunk(0,1),right-bottom chunk(1,1).(0,1)dependencies (0,1) and (1,1),(0,1) and (1,1) can compute imeadiately.You should use channels to syncronize computation,please completes the func 'chunk' to pass the test"
-`)
-	}
+	println(`Go's field is backend.In this exercise,we focus on web framework,it shortens our development time and reduces our coding work.
+Currently,there are 2 mainstream implementations,function handler and struct handler.
+We will implement a function handler tiny webframework.
+The first part is context management.Next part will focus on middleware`)
 }
