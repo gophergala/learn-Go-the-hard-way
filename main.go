1755ef7b19906273dca820ad448b9f3588bee25c
ggaaooppeenngg <471160171@qq.com>
2015-02-17 12:57:54 +0800

    fix #1,#2

--- main.go
+++ main.go
@@ -1,15 +1,66 @@
 package main
 
-import ()
+import (
+	"fmt"
+	"sync"
+)
 
-func AddPhtoFrame() {
-	//TODO:user the res/gophergala.jpg to generate a image and write to res/m.jpg which is similar to like the logo in the README.md
+// Parallelsum does parallel vector sum,
+// in each loop,buffered input capacity will be cut half
+// for the next loop to Sum goroutine to consume.
+// it terminates untill the input remains one.
+func ParallelSum(slcs ...[]int) []int {
+	input := make(chan []int, len(slcs))
+	output := make(chan []int)
+	var result []int
+	go func(input chan []int) {
+		for _, slc := range slcs {
+			input <- slc
+		}
+		close(input)
+	}(input)
 
+	for {
+		var wg sync.WaitGroup
+		wg.Add(cap(input) / 2)
+		for i := 0; i < cap(input)/2; i++ {
+			out := Sum(input)
+			go func() {
+				defer wg.Done()
+				for o := range out {
+					output <- o
+				}
+			}()
+		}
+		go func(output chan []int) {
+			wg.Wait()
+			close(output)
+		}(output)
+
+		input = make(chan []int, cap(input)/2)
+		if cap(input) < 2 {
+			result = <-output
+			break
+		}
+		for o := range output {
+			input <- o
+		}
+		output = make(chan []int)
+		close(input)
+	}
+	return result
 }
 
+//TODO:complete the Sum for the parallel sum function.
+var Sum func(sum chan []int) (output chan []int)
+
 func main() {
-	AddPhtoFrame()
-	println(`This final exercise,let's add a photo frame for gala logo!
-You should use image package to generate a new iamge from the gala log(which is stored in res/gophergala.jpg,and makes it like the res/m.jpg.
-Now edit main.go to complete 'AddPhtoFrame' function,this task has no test,enjoy your trip!`)
+	fmt.Println(`Please edit main.go,and complete the 'Sum' function for the parallel sum to pass the test.
+Concurrency is the most important feature of Go,and the principle is
+'Do not communicate by sharing memory; instead, share memory by communicating.'
+In this exercise you need to catch many features of channels.This is a tour for you to figure out!
+Because here the focus is pipleline model (link:http://blog.golang.org/pipelines).
+It's different from the custom parallel vector sum in which sum numer at every index of the vectors in a goroutine.
+In this exercies,vector is just a abstract,you can change it to a struct or any thing else that can be sumed up.
+`)
 }
--- main_test.go
+++ main_test.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestParallelSum(t *testing.T) {
+	var slice1 = []int{0, 1, 2}
+	var slice2 = []int{2, -1, 7}
+	var slice4 = []int{2, -1, 7}
+	result := ParallelSum(slice1, slice2, slice4)
+	if result[0] != 4 || result[1] != -1 || result[2] != 16 || len(result) != 3 {
+		t.Fail()
+	}
+}
