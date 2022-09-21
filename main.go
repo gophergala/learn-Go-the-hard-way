c64e8a9fffedb40fa2a0e7ddb56b6c2c049e0284
Abdullah Khabir <abdullah@abdullah.solutions>
2022-09-21 16:30:31 +0500

    fixed typo in main.go

--- main.go
+++ main.go
@@ -1,7 +1,5 @@
 package main
 
-import ()
-
 func AddPhtoFrame() {
 	//TODO:user the res/gophergala.jpg to generate a image and write to res/m.jpg which is similar to like the logo in the README.md
 
@@ -10,6 +8,6 @@ func AddPhtoFrame() {
 func main() {
 	AddPhtoFrame()
 	println(`This final exercise,let's add a photo frame for gala logo!
-You should use image package to generate a new iamge from the gala log(which is stored in res/gophergala.jpg,and makes it like the res/m.jpg.
+You should use image package to generate a new image from the gala logo(which is stored in res/gophergala.jpg,and makes it like the res/m.jpg.
 Now edit main.go to complete 'AddPhtoFrame' function,this task has no test,enjoy your trip!`)
 }
